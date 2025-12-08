042c1ef9dfaf4190cec5d8034d6278693503e50c
Ed Wright <edwright@ntlworld.com>
2025-12-08 14:58:35 +0000

    chore: add perfomance testing with lighthouse to config

---
+++ lighthouserc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: ["http://localhost:3000"],
+      startServerCommand: "npm run build && npm run start",
+      startServerReadyPattern: "Ready on",
+      numberOfRuns: 3,
+    },
+    assert: {
+      assertions: {
+        "categories:performance": ["error", { minScore: 0.8 }],
+        "categories:accessibility": ["error", { minScore: 0.9 }],
+        "categories:best-practices": ["error", { minScore: 0.9 }],
+        "categories:seo": ["error", { minScore: 0.8 }],
+        "first-contentful-paint": ["error", { maxNumericValue: 2000 }],
+        "largest-contentful-paint": ["error", { maxNumericValue: 2500 }],
+        "cumulative-layout-shift": ["error", { maxNumericValue: 0.1 }],
+      },
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+  },
+};
